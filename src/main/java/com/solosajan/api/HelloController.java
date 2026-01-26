66e6fa0444775e708401676dbb1354fa8e88b3b0
Sajan KP <sajankay09@gmail.com>
2026-01-26 18:18:18 +0000

    Add Spring Boot sample API, Docker, Helm, and CI/CD

---
+++ src/main/java/com/solosajan/api/HelloController.java
@@ -0,0 +1,19 @@
+package com.solosajan.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "API is running fine";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from GKE Spring Boot API!";
+    }
+}
+
